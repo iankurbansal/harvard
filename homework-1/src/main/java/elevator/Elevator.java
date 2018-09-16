0fed4991cd9429b57e57bbee9dbdcd4cd9872799
Ankur Bansal <ankur.a.bansal@ericsson.com>
2018-09-16 01:05:52 +0100

    HW-1 first check in

---
+++ homework-1/src/main/java/elevator/Elevator.java
@@ -0,0 +1,12 @@
+package elevator;
+
+public class Elevator {
+    private static final byte NO_OF_FLOORS_IN_BUILDING = 7;
+    private byte currentFloor;
+    private boolean isGoingUp;
+    private short[] passangersOnEachFloor;
+
+    public void move(){
+
+    }
+}
